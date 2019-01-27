0bb7e158ea7e454c5e87db356a8f8c4a782aab41
John Buhl <john.buhl@gmail.com>
2019-01-27 16:57:32 -0600

    adding notes

--- fn-stats.py
+++ fn-stats.py
@@ -1,3 +1,46 @@
+'''
+Get a list of usernames 
+Call fortnite api with each username
+Create HTML template in order to put data retrieved from api call
+Save file
+
+Do this:
+
+myList = "spellcheck,ninja"
+for i in myList:
+	response = callFunctionToAPI(i)
+	kills = getKills(response)
+	wins = getWins(response)
+
+
+totalWins=50
+totalKills=10
+
+myString = f"""
+
+<tr>
+	<td>Name</td>
+	<td>Kills</td>
+	<td>Wins</td>
+</tr>
+
+<tr>
+	<td>{username}</td>
+	<td>{totalWins}</td>
+	<td>{totalKills}</td>
+</tr>
+"""
+
+print(myString)
+
+
+'''
+
+
+
+
+
+
 import sys
 from pprint import pprint
 import requests
@@ -6,6 +49,7 @@ import json
 print(sys.version)
 
 username="spelchekk"
+
 
 url ="https://api.fortnitetracker.com/v1/profile/pc/" + username
 headers = {'TRN-Api-Key': 'e79ac99b-1033-43a8-833a-c2733ace72f6'}
@@ -33,4 +77,29 @@ print(totalWins)
 print(totalKills)
 
 #print(parsed_json['accountId'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

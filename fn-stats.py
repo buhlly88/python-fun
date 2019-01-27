783147d9145b8684800b900bec941b37958891b9
John Buhl <john.buhl@gmail.com>
2019-01-27 16:40:53 -0600

    work with api fortnite

--- fn-stats.py
+++ fn-stats.py
@@ -12,8 +12,25 @@ headers = {'TRN-Api-Key': 'e79ac99b-1033-43a8-833a-c2733ace72f6'}
 
 
 r = requests.get(url, headers=headers)
+data = r.content.decode("utf-8")
 
-parsed_json = json.loads(r)
+#print(r.content.decode("utf-8"))
+#sys.exit('bam')
 
-print(parsed_json['accountId'])
+parsedData = json.loads(data)
+
+kdP2=parsedData['stats']['p2']['kd']['value']
+kdP9=parsedData['stats']['p9']['kd']['value']
+kdP10=parsedData['stats']['p10']['kd']['value']
+totalWins=parsedData['lifeTimeStats'][8]['value']
+totalKills=parsedData['lifeTimeStats'][10]['value']
+
+
+print(kdP2)
+print(kdP9)
+print(kdP10)
+print(totalWins)
+print(totalKills)
+
+#print(parsed_json['accountId'])
 
